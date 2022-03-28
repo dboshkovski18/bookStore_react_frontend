5287d6616ec5526241ae5b28715b8c2b6cb3262f
dboskovski18 <79974565+dboshkovski18@users.noreply.github.com>
2022-03-29 00:39:55 +0200

    Update custom_axios.js

--- src/custom-axios/custom_axios.js
+++ src/custom-axios/custom_axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : "http://localhost:8080/api",
+    baseURL : "https://bookstore-spring-boot.herokuapp.com/api",
     headers : {
         'Access-Control-Allow-Origin' : '*'
     }
