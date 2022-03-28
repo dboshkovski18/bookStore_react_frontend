716de91de39f64d6077ffafd24266ec5fce051a4
dboskovski18 <79974565+dboshkovski18@users.noreply.github.com>
2022-03-29 00:37:27 +0200

    Update header.js

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -4,34 +4,7 @@ import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
 
 const header = () =>{
     return (
-        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        //     <Link className="navbar-brand" to={"/books"}>BookStore.mk</Link>
-        //     <button className="navbar-toggler" type="button" data-toggle="collapse"
-        //             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        //             aria-label="Toggle navigation">
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-        //
-        //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        //         <ul className="navbar-nav ms-auto">
-        //             <li className="nav-item active">
-        //                 <Link to={"/books"} className="nav-link" >Books</Link>
-        //             </li>
-        //             <li className="nav-item dropdown">
-        //                 <Link className={"nav-link"} to={"/categories"}>Categories</Link>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <Link className="nav-link" to={"/authors"}>Authors</Link>
-        //             </li>
-        //             <li className="nav-item dropdown">
-        //               <Link className={"nav-link"} to={"/countries"}>Countries</Link>
-        //             </li>
-        //
-        //         </ul>
-        //     </div>
-        // </nav>
-
-        <Navbar bg="light" expand="lg">
+         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="/">Bookstore.mk</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
