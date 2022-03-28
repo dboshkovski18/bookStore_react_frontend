8b80bc23eb3ac8f4e714ec91616cadf85b5dc862
Damjan <damjanx34@gmail.com>
2022-03-29 00:30:27 +0200

    Navigation bar update!

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
 
 ReactDOM.render(
   <React.StrictMode>
